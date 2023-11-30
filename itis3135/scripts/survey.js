fd474d7dd536bdbaf5dc272a0ee9f96bce1be453
jmudani <jmudani@uncc.edu>
2023-11-30 01:51:27 -0500

    byo intro assignment

---
+++ itis3135/scripts/survey.js
@@ -0,0 +1,84 @@
+function updateDOMFromFormData(data) {
+    const header = data.get('name') + ' || ' + data.get('mascot');
+
+    const fieldMappings = {
+        caption1: 'caption2',
+        personalBackground1: 'personalBackground2',
+        professionalBackground1: 'professionalBackground2',
+        academicBackground1: 'academicBackground2',
+        subjectBackground1: 'subjectBackground2',
+        compPlatform1: 'compPlatform2',
+        funnyOrInteresting1: 'funnyOrInteresting2',
+        share1: 'share2'
+    };
+    
+    for (const [fieldName, elementId] of Object.entries(fieldMappings)) {
+        const fieldValue = data.get(fieldName);
+        document.getElementById(elementId).innerText = fieldValue;
+    }
+
+    const courseList = document.getElementById('courses');
+    const courseNames = data.getAll('coursename');
+    const courseDescriptions = data.getAll('coursedescription');
+    courseList.innerHTML = '';
+
+    for (let i = 0; i < courseNames.length; i++) {
+        const item = document.createElement('li');
+        item.innerHTML = `<b>${courseNames[i]}:</b> ${courseDescriptions[i]}`;
+        courseList.appendChild(item);
+    }
+
+    const picture = document.getElementById('image').files[0];
+    if (picture) {
+        const url = URL.createObjectURL(picture);
+        document.getElementById('byoImage').src = url;
+    }
+
+    const classNamesToUpdate = ['byoHeader2', 'byoHeader3', 'byoForm', 'intro'];
+    classNamesToUpdate.forEach(className => {
+        document.getElementById(className).className = className.endsWith('New') ? className : '';
+    });
+}
+
+function submit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    updateDOMFromFormData(data);
+}
+
+function back() {
+    const courseList = document.getElementById('courses');
+    courseList.innerHTML = '';
+
+    const classNamesToUpdate = ['byoHeader2', 'byoHeader3', 'byoForm', 'intro'];
+    classNamesToUpdate.forEach(className => {
+        document.getElementById(className).className = className;
+    });
+}
+
+function addCourse(event) {
+    event.preventDefault();
+    const count = document.getElementsByClassName('course').length / 2 + 1;
+
+    ['coursename', 'coursedescription'].forEach(inputName => {
+        const label = document.createElement('label');
+        label.for = inputName;
+        label.innerText = `Course ${count} ${inputName === 'coursename' ? 'Name' : 'Description'}`;
+
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.name = inputName;
+        input.id = inputName;
+
+        const div = document.createElement('div');
+        div.appendChild(label);
+        div.appendChild(input);
+        div.className = 'course';
+
+        document.getElementById('inputContainer').appendChild(div);
+    });
+}
+
+document.getElementById('byoForm').addEventListener('submit', submit);
+document.getElementById('back').addEventListener('click', back);
+document.getElementById('addCourse').addEventListener('click', addCourse);
