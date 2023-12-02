34e212868c6348940178f763166b2532ac403e1e
jmudani <jmudani@uncc.edu>
2023-12-01 20:56:48 -0500

    calc part 2

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -21,3 +21,25 @@ document.addEventListener('keydown', function (event) {
         calculateResult();
     }
 });
+
+/* This is the calculator code for after Calculator Part 2 - Separation of Concerns */
+function setupCalculatorButtons() {
+    var buttons = document.getElementsByClassName("calculatorButton");
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            var buttonText = this.innerText;
+            if (buttonText === "C") {
+                clearDisplay();
+            } else if (buttonText === "=") {
+                calculateResult();
+            } else {
+                appendToDisplay(buttonText);
+            }
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setupCalculatorButtons();
+});
