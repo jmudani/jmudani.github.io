7facfbea9859c7c4690cec317c759e15fcf9c04e
jmudani <jmudani@uncc.edu>
2023-12-10 20:23:39 -0500

    Client Project Update!

---
+++ itis3135/clientProject/scripts/slideshow.js
@@ -0,0 +1,28 @@
+let index = 1;
+displaySlides(index);
+
+function nextSlides(n) {
+  displaySlides(index += n);
+}
+
+function currentSlide(n) {
+  displaySlides(index = n);
+}
+
+function displaySlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slidesNba");
+  let demo = document.getElementsByClassName("demo");
+  let caption = document.getElementById("caption");
+  if (n > slides.length) {index = 1}
+  if (n < 1) {index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < demo.length; i++) {
+    demo[i].className = demo[i].className.replace(" active", "");
+  }
+  slides[index-1].style.display = "block";
+  demo[index-1].className += " active";
+  caption.innerHTML = demo[index-1].alt;
+}
